77fa769b73e99d7d8ddcd0d59ee8e5074f9eec7d
Arkpatel24 <pate1616@sheridancollege.com>
2019-05-15 14:39:03 -0400

    Student info

---
+++ src/studentinfo/StudentInfo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentinfo;
+import java.util.Scanner;
+/**
+ *
+ * @author ARK PATEL
+ */
+public class StudentInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        studentTester s1= new studentTester();
+        studentTester s2= new studentTester();
+        Scanner input=new Scanner(System.in);
+        studentTester[] list=new studentTester[2];
+        
+        System.out.print("Enter first student name:");
+        s1.setName(input.nextLine());
+        System.out.println("Fisrt student is "+s1.getName());
+        
+        s2.setName("AKSHAY");
+        System.out.println("Second student is "+s2.getName());
+        
+        list[0]=s1;
+        list[1]=s2;
+        for(int i=0;i<list.length;i++)
+        {
+            System.out.println(list[i].getName());
+        }
+    }
+    
+}
---
+++ src/studentinfo/studentTester.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentinfo;
+
+/**
+ *
+ * @author ARK PATEL
+ */
+public class studentTester 
+{
+    private String _name;
+    
+    public void setName(String name)
+    {
+        this._name=name;
+    }
+    
+    public String getName()
+    {
+        return this._name;
+    }
+}
