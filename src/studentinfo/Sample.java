e330d95f575a8dca2aeb84ba4edffb2b5da10813
Arkpatel24 <pate1616@sheridancollege.com>
2019-05-22 14:00:06 -0400

    Student info

---
+++ src/studentinfo/Sample.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentinfo;
+
+/**
+ *
+ * @author ARK PATEL
+ */
+public class Sample 
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+}
